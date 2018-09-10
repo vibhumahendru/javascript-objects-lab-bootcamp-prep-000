65c720539b14591f12397ab99d0237d12169eb96
vibhu mahendru <vibhumahendru@gmail.com>
2018-09-10 20:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ return Object.assign({}, object, { [key]: value })
   
 }
 
+<<<<<<< HEAD
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
    object[key] = value
    return object
@@ -22,3 +23,5 @@ function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
   return object
 }
+=======
+>>>>>>> 6344aba6e57bebe4d6f3548f2402b1fb365c2b3e
